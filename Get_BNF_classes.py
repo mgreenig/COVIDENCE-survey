d8f9208ddaea673233ad9cc3d2c197ac179872a3
Matt <matt.greenig@gmail.com>
2020-09-17 17:23:53 +0100

    BNF pull script shortened

--- Get_BNF_classes.py
+++ Get_BNF_classes.py
@@ -2,7 +2,6 @@ from urllib.request import urlopen
 from urllib.parse import quote
 from bs4 import BeautifulSoup as bs
 import pandas as pd
-import itertools
 import re
 import pickle
 
@@ -56,47 +55,8 @@ if __name__ == '__main__':
     bnf_classes = bnf_classes.rename_axis('drugs').reset_index()
     bnf_classes['drugs'] = bnf_classes['drugs'].str.split('\s+with\s+|\s+and\s+')
     bnf_classes['drugs'] = bnf_classes['drugs'].apply(lambda drugs: [drug.strip() for drug in drugs])
-# otherwise just import
-else:
-    bnf_classes = pd.read_csv('data/bnf_drug_classifications.csv')
-    bnf_classes['drugs'] = bnf_classes['drugs'].str.split('; ')
-
-# test whether each bnf drug is in the drug dictionary
-not_found_in_drugbank = bnf_classes['drugs'].apply(lambda drugs: [drug not in drug_dictionary for drug in drugs])
-
-# get mapped and unmapped bnf drugs
-unmapped_bnf_drugs = set()
-mapped_bnf_drugs = set()
-for drug, mask in zip(bnf_classes['drugs'], not_found_in_drugbank):
-    unmapped = set(itertools.compress(drug, mask))
-    mapped = set(itertools.compress(drug, [not unmapped for unmapped in mask]))
-    unmapped_bnf_drugs = unmapped_bnf_drugs.union(unmapped)
-    mapped_bnf_drugs = mapped_bnf_drugs.union(mapped)
-
-# map first word of each drug entry in the bnf
-first_name_unmapped = []
-first_name_mapped = []
-for drug in unmapped_bnf_drugs:
-    first_part = drug.split(' ')[0]
-    if first_part in drug_dictionary:
-        drug_dictionary[drug] = drug_dictionary[first_part]
-        first_name_mapped.append(drug)
-    else:
-        first_name_unmapped.append(drug)
-
-print('{} BNF drugs mapped to DrugBank'.format(len(mapped_bnf_drugs) + len(first_name_mapped)))
-print('{} BNF drugs unmapped'.format(len(first_name_unmapped)))
-
-BNF_mapping_counts = {'exact': len(mapped_bnf_drugs), 'first_name': len(first_name_mapped), 'unmapped': len(first_name_unmapped)}
-
-if __name__ == '__main__':
-    # map bnf columns to drugbank ids
-    bnf_classes['db_id'] = bnf_classes['drugs'].apply(lambda drugs: [drug_dictionary.get(drug) for drug in drugs if drug_dictionary.get(drug)])
-    # take the union of the set of ids
-    bnf_classes['db_id'] = bnf_classes['db_id'].apply(lambda ids: set().union(*ids))
 
     # join columns that contain lists/sets
-    bnf_classes['db_id'] = bnf_classes['db_id'].str.join('; ')
     bnf_classes['drugs'] = bnf_classes['drugs'].str.join('; ')
     bnf_classes['primary'] = bnf_classes['primary'].str.join('; ')
     bnf_classes['secondary'] = bnf_classes['secondary'].str.join('; ')
