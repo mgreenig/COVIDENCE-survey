350e55fb35fc9b6da7d821b27616887bb3023ccb
Matt <matt.greenig@gmail.com>
2020-09-17 17:21:32 +0100

    class structure for patient annotator and dosage scaler added

--- Annotate_patient_dosages.py
+++ Annotate_patient_dosages.py
@@ -2,145 +2,174 @@ import pandas as pd
 import numpy as np
 from scipy.stats import zscore, norm
 
+# import the relevant data
 from Map_survey_answers import q142_cleaned, q143_dosages, q143_units, survey_data
-from Annotate_patients import drug_dictionary, bnf_classes, drug_classes, specific_drugs
+from Annotate_patients import PatientAnnotator, drug_dictionary, drug_classes, specific_drugs
 
-# dictionary for patient drug classes
-drug_class_doses = {}
+# class for scaling dosage values
+class DosageScaler(PatientAnnotator):
 
-# dictionary for patient drugs
-specific_drug_doses = {}
+    # class takes in a medication question, a dosage question, and a units question
+    def __init__(self, meds, drug_dict, dosages, units):
+        '''
+        The meds, dosages, and units series should be on the same multi-index of the form (patient_number, question_number)
 
-# get DB ids for each survey answer
-q142_db_ids = q142_cleaned.apply(lambda answer: drug_dictionary.get(answer) if drug_dictionary.get(answer) else set())
+        We use the first five letters of the question number index to distinguish between med, dosage, and unit questions, e.g.
+        - q1421_x_x -> meds
+        - q1431_x_x -> dosage values
+        - q1432_x_x -> dosage units
+        '''
+        # initialise parent class to call read_bnf()
+        super().__init__(meds, drug_dict)
+        self.med_db_ids = meds.apply(lambda answer: drug_dict.get(answer) if drug_dict.get(answer) else set())
+        self.dosages = dosages
+        self.units = units
 
-# function for aligning question indices in a survey answer mask to another survey answer series
-def align_mask(mask, series):
-    # get the question patterns from of the indices both the mask and answer series
-    q1_pattern = mask.index[0][1][:5]
-    q2_pattern = series.index[0][1][:5]
-    # rename the question index in the mask
-    mask_renamed = mask.rename({q_id: q_id.replace(q1_pattern, q2_pattern) for q_id in mask.index.get_level_values(1).unique()}, level = 1)
-    # align it to the series
-    _, mask_aligned = series.align(mask_renamed, fill_value = False)
-    return mask_aligned
+    # function for aligning question indices in a survey answer mask to another survey answer series
+    @staticmethod
+    def align_mask(mask, series):
+        # get the question patterns from of the first five letters of the mask and answer series indices
+        q1_pattern = mask.index[0][1][:5]
+        q2_pattern = series.index[0][1][:5]
+        # rename the question index in the mask
+        mask_renamed = mask.rename({q_id: q_id.replace(q1_pattern, q2_pattern) for q_id in mask.index.get_level_values(1).unique()}, level = 1)
+        # align it to the series
+        _, mask_aligned = series.align(mask_renamed, fill_value = False)
+        return mask_aligned
 
-# function for standardising drug q143_dosages for a drugbank ID
-def get_normalised_dosages(id):
+    # function for standardising drug q143_dosages for a drugbank ID
+    def get_normalised_dosages(self, id):
 
-    # if a single id is provided, convert into a set
-    ids = set([id]) if isinstance(id, str) else id
+        # if a single id is provided, convert into a set
+        ids = set([id]) if isinstance(id, str) else id
 
-    # make copy of the dosage question so the global object is not modified
-    dosages = q143_dosages.copy()
-    # filter for answers with the same DB id(s)
-    drug_mask = q142_db_ids.apply(lambda drug_ids: True if ids == drug_ids else False)
-    # change index name
-    drug_mask = align_mask(drug_mask, q143_units)
+        # make copy of the dosage question so the global object is not modified
+        dosages = self.dosages.copy()
+        # filter for answers with the same DB id(s)
+        drug_mask = self.med_db_ids.apply(lambda drug_ids: True if ids == drug_ids else False)
+        # change index name
+        drug_mask = DosageScaler.align_mask(drug_mask, self.units)
 
-    # if any drugs are hit, get the dosages
-    if drug_mask.any():
+        # if any drugs are hit, get the dosages
+        if drug_mask.any():
 
-        # get all dosage q143_units specified for the drug
-        dosage_units = q143_units[drug_mask]
-        mg_mask = align_mask(dosage_units == 1, dosages)
-        micg_mask = align_mask(dosage_units == 2, dosages)
-        invalid_unit_mask = align_mask((dosage_units != 1) & (dosage_units != 2), dosages)
+            # get all dosage q143_units specified for the drug
+            dosage_units = self.units[drug_mask]
+            mg_mask = DosageScaler.align_mask(dosage_units == 1, dosages)
+            micg_mask = DosageScaler.align_mask(dosage_units == 2, dosages)
+            invalid_unit_mask = DosageScaler.align_mask((dosage_units != 1) & (dosage_units != 2), dosages)
 
-        # get iqr and quantiles of the mg drug q143_dosages
-        q1, q3 = dosages[mg_mask].quantile([0.25, 0.75])
-        dose_iqr = q3 - q1
+            # get iqr and quantiles of the mg drug q143_dosages
+            q1, q3 = dosages[mg_mask].quantile([0.25, 0.75])
+            dose_iqr = q3 - q1
 
-        # change the microgram values in the actual dosage values if the resulting answers are not outliers
-        dosages[micg_mask] = np.where((dosages[micg_mask] / 1000 < q1-dose_iqr) | (dosages[micg_mask] / 1000 > q3+dose_iqr),
-                                      dosages[micg_mask], dosages[micg_mask] / 1000)
+            # change the microgram values in the actual dosage values if the resulting answers are not outliers
+            dosages[micg_mask] = np.where((dosages[micg_mask] / 1000 < q1-dose_iqr) | (dosages[micg_mask] / 1000 > q3+dose_iqr),
+                                          dosages[micg_mask], dosages[micg_mask] / 1000)
 
-        # get all dosage values with valid dosage units
-        valid_unit_mask = mg_mask | micg_mask
-        valid_dosages = dosages[valid_unit_mask]
+            # get all dosage values with valid dosage units
+            valid_unit_mask = mg_mask | micg_mask
+            valid_dosages = dosages[valid_unit_mask]
 
-        # calculate z score
-        valid_dosages_scaled = zscore(valid_dosages)
+            # calculate z score
+            valid_dosages_scaled = zscore(valid_dosages)
 
-        # normalised using probit function
-        valid_dosages_norm = pd.Series(norm.cdf(valid_dosages_scaled), index = valid_dosages.index)
+            # normalised using probit function
+            valid_dosages_norm = pd.Series(norm.cdf(valid_dosages_scaled), index = valid_dosages.index)
 
-        # get invalid dosages
-        invalid_dosages = dosages[invalid_unit_mask].apply(lambda dosage: 'NA')
+            # get invalid dosages
+            invalid_dosages = dosages[invalid_unit_mask].apply(lambda dosage: 'NA')
 
-        # combine the two dosage lists
-        all_dosages = valid_dosages_norm.append(invalid_dosages)
-        all_dosages = all_dosages.sort_index()
+            # combine the two dosage lists
+            all_dosage_values = valid_dosages_norm.append(invalid_dosages)
+            all_dosage_values = all_dosage_values.sort_index()
+            all_dosage_values[all_dosage_values == -99] = 'NA'
 
-        # align to the larger series
-        _, drug_dosages_aligned = dosages.align(all_dosages, fill_value=0)
+            # align to the larger series
+            _, drug_dosages_aligned = dosages.align(all_dosage_values, fill_value=0)
 
-    # otherwise save the dosages as a series of 0
-    else:
-        drug_dosages_aligned = pd.Series(0, index = dosages.index)
+        # otherwise save the dosages as a series of 0
+        else:
+            drug_dosages_aligned = pd.Series(0, index = dosages.index)
 
-    return drug_dosages_aligned
+        return drug_dosages_aligned
 
-# function for checking if a series of dosage answers consists of only NA and 0 values
-is_na = lambda row: False if all(row == 0) else False if all(row != 'NA') else True
+    @staticmethod
+    # function for checking if a series of dosage answers consists of only NA and 0 values
+    def is_na(row):
+        return False if all(row == 0) else False if all(row != 'NA') else True
 
-# function for combining dosage data across multiple drugs into a single value (either a sum or NA)
-combine_func = lambda row: 'NA' if is_na(row) else row[row != 'NA'].sum()
+    @staticmethod
+    # function for combining dosage data across multiple drugs into a single value (either a sum or NA)
+    def combine_func(row):
+        return 'NA' if DosageScaler.is_na(row) else row[row != 'NA'].sum()
 
-# function for getting the dosage values for each class
-def get_class_doses(drug_class):
+    # function for getting the dosage values for each class
+    def get_class_doses(self, drug_class):
 
-    # get bnf entries that fall into the desired drug class
-    drug_class_mask = bnf_classes['primary'].str.contains(drug_class) | bnf_classes['secondary'].str.contains(drug_class)
-    class_drugs = bnf_classes[drug_class_mask]
+        # filter for BNF listings that mapped to the drug dictionary
+        valid_bnf_classes = self.bnf_classes[self.bnf_classes['db_id'].apply(lambda ids: len(ids) > 0)]
 
-    # isolate entries that in the drug class that correspond to single drugbank IDs, to avoid ambiguity with mixture products
-    single_id_mask = class_drugs['db_id'].apply(lambda ids: len(ids) == 1)
-    # get the db ids corresponding to the drug class
-    class_db_ids = set().union(*class_drugs['db_id'][single_id_mask])
+        # get bnf entries that fall into the desired drug class
+        drug_class_mask = valid_bnf_classes['primary'].str.contains(drug_class) | valid_bnf_classes['secondary'].str.contains(drug_class)
+        class_drugs = valid_bnf_classes[drug_class_mask]
 
-    # loop through ids in the class and add dosages to the dictionary of dosage data
-    all_dosages = {}
-    for id in class_db_ids:
-        dosages = get_normalised_dosages(id)
-        all_dosages[id] = dosages
+        # isolate entries that in the drug class that correspond to single drugbank IDs, to avoid ambiguity with mixture products
+        single_id_mask = class_drugs['db_id'].apply(lambda ids: len(ids) == 1)
+        # get the db ids corresponding to the drug class
+        class_db_ids = set().union(*class_drugs['db_id'][single_id_mask])
 
-    # make data frame from the dosage data for all drugs in the class
-    dosage_df = pd.DataFrame(all_dosages)
+        # loop through ids in the class and add dosages to the dictionary of dosage data
+        drug_dosages = {}
+        for id in class_db_ids:
+            dosages = self.get_normalised_dosages(id)
+            drug_dosages[id] = dosages
 
-    # group by patient, summing to get the total dose per patient within each class
-    question_dosages = dosage_df.apply(combine_func, axis = 1)
-    patient_dosages = question_dosages.groupby(level = 0).apply(combine_func)
+        # make data frame from the dosage data for all drugs in the class
+        dosage_df = pd.DataFrame(drug_dosages)
 
-    # align to the total set of survey answers
-    _, patient_dosages_aligned = survey_data.align(patient_dosages, axis = 0, fill_value = 0)
+        # group by patient, summing to get the total dose per patient within each class
+        question_dosages = dosage_df.apply(DosageScaler.combine_func, axis = 1)
+        patient_dosages = question_dosages.groupby(level = 0).apply(DosageScaler.combine_func)
 
-    return patient_dosages_aligned
+        # align to the total set of survey answers
+        _, patient_dosages_aligned = survey_data.align(patient_dosages, axis = 0, fill_value = 0)
 
-# function for getting the class
-def get_drug_doses(drug):
+        return patient_dosages_aligned
 
-    # get the drugbank id corresponding to the drug of interest
-    drug_db_ids = drug_dictionary[drug]
-    # get the normalised dosages
-    dosages = get_normalised_dosages(drug_db_ids)
-    # group by patient, summing to get the total dose per patient within each class
-    patient_dosages = dosages.groupby(level = 0).apply(combine_func)
-    # align to the total set of survey answers
-    _, patient_dosages_aligned = survey_data.align(patient_dosages, axis = 0, fill_value = 0)
+    # function for getting drug dosages
+    def get_drug_doses(self, drug):
 
-    return patient_dosages_aligned
+        # get the drugbank id corresponding to the drug of interest
+        drug_db_ids = drug_dictionary[drug]
+        # get the normalised dosages
+        dosages = self.get_normalised_dosages(drug_db_ids)
+        # group by patient, summing to get the total dose per patient within each class
+        patient_dosages = dosages.groupby(level = 0).apply(self.combine_func)
+        # align to the total set of survey answers
+        _, patient_dosages_aligned = survey_data.align(patient_dosages, axis = 0, fill_value = 0)
+
+        return patient_dosages_aligned
 
 
 if __name__ == '__main__':
 
+    # dictionary for patient drug classes
+    drug_class_doses = {}
+
+    # dictionary for patient drugs
+    specific_drug_doses = {}
+
+    # make a class instance
+    scaler = DosageScaler(q142_cleaned, drug_dictionary, q143_dosages, q143_units)
+
     # label patient drug classes
     for drug_class in drug_classes:
-        drug_class_doses[drug_class] = get_class_doses(drug_class)
+        drug_class_doses[drug_class] = scaler.get_class_doses(drug_class)
 
     # label patient drugs
     for drug in specific_drugs:
-        specific_drug_doses[drug] = get_drug_doses(drug)
+        specific_drug_doses[drug] = scaler.get_drug_doses(drug)
 
     # put all the patient features into a single dictionary
     patient_dose_feature_dict = {**drug_class_doses, **specific_drug_doses}
--- Annotate_patients.py
+++ Annotate_patients.py
@@ -1,23 +1,17 @@
 import pandas as pd
 import warnings
-import numpy as np
+from itertools import compress
 warnings.simplefilter('ignore')
 
+# import drug dictionary and survey answers
 from Map_survey_answers import q142_cleaned, survey_data, drug_dictionary
 
-# import bnf class dataframe
-bnf_classes = pd.read_csv('data/bnf_drug_classifications.csv')
-# filter for drugs with drugbank ids
-bnf_classes = bnf_classes[~bnf_classes['db_id'].isna()]
-# split the drugbank ids to produce sets
-bnf_classes['db_id'] = bnf_classes['db_id'].apply(lambda ids: set(ids.split('; ')))
-# list of drugbank ids mapped to bnf classes
-bnf_db_ids = set().union(*bnf_classes['db_id'].tolist())
-
-# manual id setting for vitamins
+# manual id setting for a few medications
 drug_dictionary['vitamin b12'] = {'DB00115'}
 drug_dictionary['vitamin e'] = {'DB00163'}
 drug_dictionary['vitamin d'] = {'DB00136', 'DB00153', 'DB00169', 'DB00910', 'DB01070', 'DB01436', 'DB02300', 'DB13689'}
+drug_dictionary['candesartan cilexetil'] = {'DB13919'}
+drug_dictionary['candesartan'] = {'DB13919'}
 
 # load in unmapped answers and map to the drug dictionary
 corrections = pd.read_csv('data/answer_mappings_complete.csv')
@@ -40,64 +34,125 @@ drug_classes = ['statins', 'ace inhibitors', 'proton pump inhibitors', 'corticos
 
 specific_drugs = ['paracetamol', 'metformin', 'aspirin']
 
-# dictionary for patient drug classes
-patient_drug_class_dict = {}
+class PatientAnnotator:
 
-# dictionary for patient drugs
-patient_drug_dict = {}
+    # function for reading in BNF data and annotating table with DB ids
+    def read_in_bnf(self, drug_dictionary):
 
-# function for updating dictionary with a list of patient indices belonging to a drug class
-def get_patients_in_class(drug_class, patient_answers):
+        # import bnf class dataframe
+        bnf_classes = pd.read_csv('data/bnf_drug_classifications.csv')
+        bnf_classes['drugs'] = bnf_classes['drugs'].str.split('; ')
 
-    # get bnf entries that fall into the desired drug class
-    drug_class_mask = bnf_classes['primary'].str.contains(drug_class) | bnf_classes['secondary'].str.contains(drug_class)
-    class_drugs = bnf_classes[drug_class_mask]
+        # map bnf columns to drugbank ids
+        bnf_classes['db_id'] = bnf_classes['drugs'].apply(
+            lambda drugs: [drug_dictionary.get(drug) for drug in drugs if drug_dictionary.get(drug)])
+        # take the union of the set of ids
+        bnf_classes['db_id'] = bnf_classes['db_id'].apply(lambda ids: set().union(*ids))
 
-    # isolate entries that in the drug class that correspond to single drugbank IDs, to avoid ambiguity with mixture products
-    single_id_mask = class_drugs['db_id'].apply(lambda ids: len(ids) == 1)
-    # get the db ids corresponding to the drug class
-    class_db_ids = set().union(*class_drugs['db_id'][single_id_mask])
+        # set class attribute
+        self.bnf_classes = bnf_classes
 
-    # function for checking if a patient's answers are in a drug class
-    is_in_class = lambda patient: patient.apply(
-        lambda answer: any([db_id in class_db_ids for db_id in drug_dictionary.get(answer)]) if drug_dictionary.get(
-            answer) else False).any()
+        # list of drugbank ids mapped to bnf classes
+        self.bnf_db_ids = set().union(*self.bnf_classes['db_id'].tolist())
 
-    # group by patient and apply the is_in_class function to each patient
-    patient_class_mask = patient_answers.groupby(level = 0).aggregate(func = is_in_class)
+    # requires a series of patient answers and a dictionary of drug aliases mapped to DB ids
+    def __init__(self, meds, drug_dict):
+        self.meds = meds
+        self.drug_dictionary = drug_dict
+        # get bnf data from the data directory
+        self.read_in_bnf(drug_dictionary)
 
-    # get the indices of patients in the class
-    patients_in_class = patient_class_mask.index[patient_class_mask].tolist()
+    # function for updating dictionary with a list of patient indices belonging to a drug class
+    def get_patients_in_class(self, drug_class):
 
-    return patients_in_class
+        valid_bnf_classes = self.bnf_classes[self.bnf_classes['db_id'].apply(lambda ids: len(ids) > 0)]
 
-# function for getting a list of patient indices taking a specific drug
-def get_patients_on_drug(drug, patient_answers):
+        # get bnf entries that fall into the desired drug class
+        drug_class_mask = valid_bnf_classes['primary'].str.contains(drug_class) | valid_bnf_classes['secondary'].str.contains(drug_class)
+        class_drugs = valid_bnf_classes[drug_class_mask]
 
-    # get the drugbank id corresponding to the drug of interest
-    drug_db_ids = drug_dictionary[drug]
+        # isolate entries that in the drug class that correspond to single drugbank IDs, to avoid ambiguity with mixture products
+        single_id_mask = class_drugs['db_id'].apply(lambda ids: len(ids) == 1)
+        # get the db ids corresponding to the drug class
+        class_db_ids = set().union(*class_drugs['db_id'][single_id_mask])
 
-    # function for assessing whether each patient takes the drug
-    takes_drug = lambda patient: patient.apply(lambda drug: drug_dictionary.get(drug).issuperset(drug_db_ids)
-                                               if drug_dictionary.get(drug) else False).any()
+        # function for checking if a patient's answers are in a drug class
+        is_in_class = lambda patient: patient.apply(
+            lambda answer: any([db_id in class_db_ids for db_id in self.drug_dictionary.get(answer)]) if self.drug_dictionary.get(
+                answer) else False).any()
 
-    # group by patient and apply the takes_drug function to each patient
-    patient_drug_mask = patient_answers.groupby(level = 0).aggregate(func = takes_drug)
+        # group by patient and apply the is_in_class function to each patient
+        patient_class_mask = self.meds.groupby(level = 0).aggregate(func = is_in_class)
 
-    # get the indices of patients taking the drug
-    patients_on_drug = patient_drug_mask.index[patient_drug_mask].tolist()
+        # get the indices of patients in the class
+        patients_in_class = patient_class_mask.index[patient_class_mask].tolist()
 
-    return patients_on_drug
+        return patients_in_class
+
+    # function for getting a list of patient indices taking a specific drug
+    def get_patients_on_drug(self, drug):
+
+        # get the drugbank ids corresponding to the drug of interest
+        drug_db_ids = self.drug_dictionary[drug]
+
+        # function for assessing whether each patient takes the drug
+        takes_drug = lambda patient: patient.apply(lambda drug: self.drug_dictionary.get(drug).issuperset(drug_db_ids)
+                                                   if self.drug_dictionary.get(drug) else False).any()
+
+        # group by patient and apply the takes_drug function to each patient
+        patient_drug_mask = self.meds.groupby(level = 0).aggregate(func = takes_drug)
+
+        # get the indices of patients taking the drug
+        patients_on_drug = patient_drug_mask.index[patient_drug_mask].tolist()
+
+        return patients_on_drug
+
+# initialise class instance
+annotator = PatientAnnotator(q142_cleaned, drug_dictionary)
+
+# test whether each bnf drug is in the drug dictionary
+not_found_in_drugbank = annotator.bnf_classes['drugs'].apply(lambda drugs: [drug not in drug_dictionary for drug in drugs])
+
+# get mapped and unmapped bnf drugs
+unmapped_bnf_drugs = set()
+mapped_bnf_drugs = set()
+for drug, mask in zip(annotator.bnf_classes['drugs'], not_found_in_drugbank):
+    unmapped = set(compress(drug, mask))
+    mapped = set(compress(drug, [not unmapped for unmapped in mask]))
+    unmapped_bnf_drugs = unmapped_bnf_drugs.union(unmapped)
+    mapped_bnf_drugs = mapped_bnf_drugs.union(mapped)
+
+# map first word of each drug entry in the bnf
+first_name_unmapped = []
+first_name_mapped = []
+for drug in unmapped_bnf_drugs:
+    first_part = drug.split(' ')[0]
+    if first_part in drug_dictionary:
+        drug_dictionary[drug] = drug_dictionary[first_part]
+        first_name_mapped.append(drug)
+    else:
+        first_name_unmapped.append(drug)
+
+print('{} BNF drugs mapped'.format(len(mapped_bnf_drugs) + len(first_name_mapped)))
+print('{} BNF drugs unmapped'.format(len(first_name_unmapped)))
+
+BNF_mapping_counts = {'exact': len(mapped_bnf_drugs), 'first_name': len(first_name_mapped), 'unmapped': len(first_name_unmapped)}
 
 if __name__ == '__main__':
 
+    # dictionary for patient drug classes
+    patient_drug_class_dict = {}
+
+    # dictionary for patient drugs
+    patient_drug_dict = {}
+
     # label patient drug classes
     for drug_class in drug_classes:
-        patient_drug_class_dict[drug_class] = get_patients_in_class(drug_class, q142_cleaned)
+        patient_drug_class_dict[drug_class] = annotator.get_patients_in_class(drug_class)
 
     # label patient drugs
     for drug in specific_drugs:
-        patient_drug_dict[drug] = get_patients_on_drug(drug, q142_cleaned)
+        patient_drug_dict[drug] = annotator.get_patients_on_drug(drug)
 
     # put all the patient features into a single dictionary
     patient_feature_dict = {**patient_drug_class_dict, **patient_drug_dict}
